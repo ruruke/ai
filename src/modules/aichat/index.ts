0b2fc45855ebf98bb2dbf2b1c5b6957b72f65556
lqvp <183242690+lqvp@users.noreply.github.com>
2025-03-02 11:54:38 +0900

    fix: 独り言が正しくノートされるように

--- src/modules/aichat/index.ts
+++ src/modules/aichat/index.ts
@@ -591,8 +591,7 @@ export default class extends Module {
     const base64Files: base64File[] = []; // 自動ノートの場合はファイルは添付しない
     const text = await this.genTextByGemini(aiChat, base64Files);
     if (text) {
-			const note = text + '#aichat';
-      this.ai.post({ text: note });
+			this.ai.post({ text: text + ' #aichat' });
     } else {
       this.log('Gemini自動ノートの生成に失敗しました。');
     }
